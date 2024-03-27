004507304dc8d906a859f8327122cd78fe5dfaf8
stormshadow96 <jletsoalo257@gmail.com>
2024-03-27 16:33:35 -0700

    updated 1-hbnb

--- web_flask/1-hbnd_route.py
+++ web_flask/1-hbnd_route.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" Starts a Flash Web Application HBNB"""
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    """Return string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return string when route queried
-    """
+    """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
